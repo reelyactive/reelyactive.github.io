e5fd03da11767d4c3d13cf11fe6f5203cb9bcb40
jeffyactive <jeff@reelyactive.com>
2016-06-01 21:36:51 -0400

    Added websockets example

---
+++ js/service.js
@@ -0,0 +1,42 @@
+DEFAULT_SOCKET_URL = 'http://www.hyperlocalcontext.com/reelyactive';
+EVENT_HISTORY = 5;
+
+angular.module('service', [ 'ui.bootstrap', 'btford.socket-io',
+                            'reelyactive.beaver'])
+
+// Socket.io factory
+.factory('Socket', function(socketFactory) {
+  return socketFactory({
+    ioSocket: io.connect(DEFAULT_SOCKET_URL)
+  });
+})
+
+// Service controller
+.controller('ServiceCtrl', function($scope, Socket, beaver) {
+  $scope.events = [];
+
+  beaver.listen(Socket);
+
+  beaver.on('appearance', function(event) {
+    updateEvents('appearance', event);
+  });
+  beaver.on('displacement', function(event) {
+    updateEvents('displacement', event);
+  });
+  beaver.on('keep-alive', function(event) {
+    updateEvents('keep-alive', event);
+  });
+  beaver.on('disappearance', function(event) {
+    updateEvents('disappearance', event);
+  });
+
+  function updateEvents(type, event) {
+    var length = $scope.events.unshift({
+      type: type,
+      tiraid: event.tiraid
+    });
+    if (length > EVENT_HISTORY) {
+      $scope.events.pop();
+    }
+  }
+});
