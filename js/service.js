d1e64123ce4b44980ff7a5bbff77f37c8e20f4f9
jeffyactive <jeff@reelyactive.com>
2017-01-18 10:55:12 -0500

    Updated to use HTTPS-HTTPS

--- js/service.js
+++ js/service.js
@@ -1,4 +1,4 @@
-DEFAULT_SOCKET_URL = 'http://www.hyperlocalcontext.com/reelyactive';
+DEFAULT_SOCKET_URL = 'https://www.hyperlocalcontext.com/reelyactive';
 EVENT_HISTORY = 5;
 
 angular.module('service', [ 'ui.bootstrap', 'btford.socket-io',
