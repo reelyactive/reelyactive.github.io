2f6d75298007e25afefd00f293658ff9e0c306c4
jeffyactive <jeff@reelyactive.com>
2017-07-17 20:38:07 -0400

    Upgraded to latest version

--- js/beaver.js
+++ js/beaver.js
@@ -1,10 +1,11 @@
 /**
- * Copyright reelyActive 2016
+ * Copyright reelyActive 2016-2017
  * We believe in an open Internet of Things
  */
 
 
-DEFAULT_ASSOCIATIONS_API_URL = 'https://www.hyperlocalcontext.com/associations/';
+DEFAULT_DISAPPEARANCE_MILLISECONDS = 15000;
+DEFAULT_POLLING_MILLISECONDS = 5000;
 
 
 angular.module('reelyactive.beaver', [])
@@ -12,100 +13,138 @@ angular.module('reelyactive.beaver', [])
   .factory('beaver', function beaverFactory($http) {
 
     var devices = {};
-    var associations = {};
+    var directories = {};
     var stats = { appearances: 0, displacements: 0, keepalives: 0,
                   disappearances: 0 };
-    var associationsApiUrl = DEFAULT_ASSOCIATIONS_API_URL;
     var eventCallbacks = {};
+    var pollingApiUrl;
+    var disappearanceMilliseconds = DEFAULT_DISAPPEARANCE_MILLISECONDS;
 
 
-    function updateDevice(event, data) {
-      if(!data || !data.tiraid) {
+    // Use the given event to update the status of the corresponding device
+    function updateDevice(type, event) {
+      if(!isValidEvent) {
         return;
       }
 
-      var tiraid = data.tiraid;
-      if(!tiraid || !tiraid.identifier || !tiraid.identifier.value) {
-        return;
+      if(!event.hasOwnProperty('deviceId') && event.hasOwnProperty('tiraid')) {
+        updateLegacyEvent(event);
       }
 
-      var id = tiraid.identifier.value;
-      if(event === 'disappearance') {
-        if(devices.hasOwnProperty(id)) {
-          // TODO: cache stats?
-          delete devices[id];
+      var deviceId = event.deviceId;
+      if(!devices.hasOwnProperty(deviceId)) {
+        type = 'appearance';
+      }
+
+      if(type === 'appearance') { stats.appearances++; }
+      if(type === 'displacement') { stats.displacements++; }
+      if(type === 'keep-alive') { stats.keepalives++; }
+      if(type === 'disappearance') {
+        if(devices.hasOwnProperty(deviceId)) {
+          delete devices[deviceId];
         }
         stats.disappearances++;
-        handleEventCallback(event, data);
+        handleEventCallback(type, event);
         return;
       }
 
-      if(!devices.hasOwnProperty(id)) {
-        devices[id] = { tiraid: tiraid };
+      if(!devices.hasOwnProperty(deviceId)) {
+        devices[deviceId] = { event:  event };
       }
       else {
-        devices[id].tiraid = tiraid;
+        mergeDeviceEvents(devices[deviceId], event);
       }
 
-      var device = devices[id];
-      if(!device.hasOwnProperty('associations')) {
-        getAssociations(id, function(id, associations) {
-          if(devices.hasOwnProperty(id)) {
-            devices[id].associations = associations;
-          }
-        });
-      }
-      addReceiverAssociations(tiraid);
-
-      if(event === 'appearance') { stats.appearances++; }
-      if(event === 'displacement') { stats.displacements++; }
-      if(event === 'keep-alive') { stats.keepalives++; }
-      handleEventCallback(event, device);
+      handleEventCallback(type, event);
     }
 
 
-    function getAssociations(id, callback) {
-      if(associations.hasOwnProperty(id)) {
-        return callback(id, associations[id]);
+    // Verify if the given event is valid
+    function isValidEvent(event) {
+      if(!event) {
+        return false;
       }
-      associations[id] = {};
-      $http.defaults.headers.common.Accept = 'application/json';
-      $http.get(associationsApiUrl + id)
-        .success(function(data, status, headers, config) {
-          if(data.devices && data.devices[id]) {
-            associations[id] = data.devices[id];
-          }
-          return callback(id, associations[id]);
-        })
-        .error(function(data, status, headers, config) {
-          return callback(id, associations[id]);
-        });
+      if(!((event.deviceId && event.receiverId && event.rssi && event.time) ||
+           (event.tiraid))) {
+        return false;
+      }
+      return true;
     }
 
 
-    function addReceiverAssociations(tiraid) {
-      for(var cDecoding = 0; cDecoding < tiraid.radioDecodings.length;
-          cDecoding++) {
-        var decoding = tiraid.radioDecodings[cDecoding];
-        var decoderId = decoding.identifier.value;
-        if(associations.hasOwnProperty(decoderId)) {
-          decoding.associations = associations[decoderId];
+    // Update the given event to the current format
+    function updateLegacyEvent(event) {
+      event.deviceId = event.tiraid.identifier.value;
+      event.time = new Date(event.tiraid.timestamp);
+      event.receiverId = event.tiraid.radioDecodings[0].identifier.value;
+      event.rssi = event.tiraid.radioDecodings[0].rssi;
+      return;
+    }
+
+
+    // Merge any previous device event with the given one
+    function mergeDeviceEvents(device, event) {
+      device.event.event = event.event;
+      device.event.time = event.time;
+      device.event.deviceAssociationIds = event.deviceAssociationIds ||
+                                          device.event.deviceAssociationIds;
+      device.event.deviceUrl = event.deviceUrl || device.event.deviceUrl;
+      device.event.deviceTags = event.deviceTags || device.event.deviceTags;
+      device.event.receiverId = event.receiverId;
+      device.event.receiverUrl = event.receiverUrl;
+      device.event.receiverTags = event.receiverTags;
+      device.event.receiverDirectory = event.receiverDirectory;
+      device.event.rssi = event.rssi;
+      device.event.rssiType = event.rssiType;
+    }
+
+
+    // Update the directories of devices
+    function updateDirectories(event) {
+      var directory = event.receiverDirectory;
+      var deviceId = event.deviceId;
+
+      for(cDirectory in directories) {
+        if((directory === cDirectory) && (event.event !== 'disappearance')) {
+          addReceiver(directory, event);
+          directories[cDirectory].devices[deviceId] = devices[deviceId];
         }
-        else {
-          getAssociations(decoderId, function(id, associations) {});
+        else if(directories[cDirectory].devices.hasOwnProperty(deviceId)) {
+          delete directories[cDirectory].devices[deviceId];
         }
+      }
+
+      if(!directories.hasOwnProperty(directory)) {
+        directories[directory] = { receivers: {}, devices: {} };
+        addReceiver(directory, event);
+        directories[directory].devices[deviceId] = devices[deviceId];
       }
     }
 
 
-    function handleEventCallback(event, device) {
-      var callback = eventCallbacks[event];
+    // Add the receiver to the given directory
+    function addReceiver(directory, event) {
+      if(!directories[directory].receivers.hasOwnProperty(event.receiverId)) {
+        directories[directory].receivers[event.receiverId] = {
+          receiverId: event.receiverId,
+          receiverTags: event.receiverTags,
+          receiverDirectory: directory,
+          receiverUrl: event.receiverUrl
+        };
+      }
+    }
+
+
+    // Handle any registered callbacks for the given event type
+    function handleEventCallback(type, device) {
+      var callback = eventCallbacks[type];
       if(callback) {
         callback(device);
       }
     }
 
 
+    // Register a callback for the given event type
     var setEventCallback = function(event, callback) {
       if(callback && (typeof callback === 'function')) { 
         eventCallbacks[event] = callback;
@@ -113,32 +152,160 @@ angular.module('reelyactive.beaver', [])
     }
 
 
-    var handleSocketEvents = function(Socket) {
+    // Add the given property to the given device
+    function addDeviceProperty(deviceId, property, value) {
+      if(!devices.hasOwnProperty(deviceId) || !property ||
+         (typeof(property) !== 'string')) {
+        return false;
+      }
+      devices[deviceId][property] = value;
+      return true;
+    }
 
-      Socket.on('appearance', function(data) {
-        updateDevice('appearance', data);
+
+    // Add the given property to the given directory
+    function addDirectoryProperty(directory, property, value) {
+      if(!directories.hasOwnProperty(directory) || !property ||
+         (typeof(property) !== 'string')) {
+        return false;
+      }
+      directories[directory][property] = value;
+      return true;
+    }
+
+
+    // Purge any stale devices as disappearances
+    function purgeDisappearances() {
+      var currentTime = new Date().getTime();
+      for(cDevice in devices) {
+        var stalenessMilliseconds = currentTime - devices[cDevice].event.time;
+        if(stalenessMilliseconds > disappearanceMilliseconds) {
+          handleEventCallback('disappearance', devices[cDevice].event);
+          delete devices[cDevice];
+          stats.disappearances++;
+        }
+      }
+    }
+
+
+    // Handle incoming socket events by type
+    var handleSocketEvents = function(Socket, options) {
+      handleOptions(options);
+
+      Socket.on('appearance', function(event) {
+        updateDevice('appearance', event);
+        updateDirectories(event);
       });
 
-      Socket.on('displacement', function(data) {
-        updateDevice('displacement', data);
+      Socket.on('displacement', function(event) {
+        updateDevice('displacement', event);
+        updateDirectories(event);
       });
 
-      Socket.on('keep-alive', function(data) {
-        updateDevice('keep-alive', data);
+      Socket.on('keep-alive', function(event) {
+        updateDevice('keep-alive', event);
+        updateDirectories(event);
       });
 
-      Socket.on('disappearance', function(data) {
-        updateDevice('disappearance', data);
+      Socket.on('disappearance', function(event) {
+        updateDevice('disappearance', event);
+        updateDirectories(event);
       });
 
       Socket.on('error', function(err, data) {
       });
+
+      var intervalMilliseconds = Math.round(disappearanceMilliseconds / 2);
+      setInterval(purgeDisappearances, intervalMilliseconds);
     };
+
+
+    // Update the given device from the list of polled devices
+    function updatePolledDevice(deviceId, polledDevices) {
+      var device = polledDevices[deviceId];
+      if(!device.hasOwnProperty('nearest')) {
+        return;
+      }
+      var receiverId = device.nearest[0].device;
+      var receiver = polledDevices[receiverId];
+      var event = {
+        deviceId: deviceId,
+        deviceAssociationIds: device.deviceAssociationIds || [],
+        deviceUrl: device.url,
+        deviceTags: device.tags,
+        receiverId: device.nearest[0].device,
+        receiverUrl: receiver.url,
+        receiverTags: receiver.tags,
+        receiverDirectory: receiver.directory,
+        rssi: device.nearest[0].rssi,
+        time: new Date().getTime()
+      };
+      var type = 'appearance';
+
+      if(devices.hasOwnProperty(deviceId)) {
+        if(devices[deviceId].event.receiverId === event.receiverId) {
+          type = 'keep-alive';
+        }
+        else {
+          type = 'displacement';
+        }
+      }
+      event.event = type;
+
+      updateDevice(type, event);
+      updateDirectories(event);
+    }
+
+
+    // Query the polling API
+    function queryApi() {
+      $http({ method: 'GET', url: pollingApiUrl })
+        .then(function(response) { // Success
+          if(response.data.hasOwnProperty('devices')) {
+            for(deviceId in response.data.devices) {
+              updatePolledDevice(deviceId, response.data.devices);
+            }
+            purgeDisappearances();
+          }
+        }, function(response) {    // Error
+          console.log('beaver: GET ' + pollingApiUrl + ' returned status ' +
+                      response.status);
+      });
+    }
+
+
+    // Initialise polling of API
+    var initPolling = function(url, interval, options) {
+      if(!url || (typeof url !== 'string')) {
+        return;
+      }
+      handleOptions(options);
+
+      $http.defaults.headers.common.Accept = 'application/json';
+      interval = interval || DEFAULT_POLLING_MILLISECONDS;
+      pollingApiUrl = url;
+
+      queryApi();
+      setInterval(queryApi, interval);
+    };
+
+
+    // Handle options provided when listening/polling, if any
+    function handleOptions(options) {
+      options = options || {};
+      disappearanceMilliseconds = options.disappearanceMilliseconds ||
+                                  DEFAULT_DISAPPEARANCE_MILLISECONDS;
+    }
+
 
     return {
       listen: handleSocketEvents,
+      poll: initPolling,
       on: setEventCallback,
+      addDeviceProperty: addDeviceProperty,
+      addDirectoryProperty: addDirectoryProperty,
       getDevices: function() { return devices; },
+      getDirectories: function() { return directories; },
       getStats: function() { return stats; }
     }
   });
